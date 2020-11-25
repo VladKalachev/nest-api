b000fa16966dc11e1b09027cf4ab722cf192d2e5
Corvax <vlvete1990@yandex.ru>
2020-11-25 18:36:28 +0300

    chore: add logics for service products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,24 +1,30 @@
 import { Body, Controller, Delete, Get, Header, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+
+    constructor(private readonly productsService: ProductsService) {
+        this.productsService = productsService
+    }
+
     @Get()
-    getAll():string {
-        return 'getAll'
+    getAll() {
+        return this.productsService.getAll()
     }
 
     @Get(':id')
-    getOne(@Param('id') id: string): string {
-        return 'getOne ' + id
+    getOne(@Param('id') id: string) {
+        return this.productsService.getById(id)
     }
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
     @Header('Cache-control', 'none')
-    create(@Body() createProductDto: CreateProductDto): string {
-        return `Title ${createProductDto.title} ${createProductDto.price}`
+    create(@Body() createProductDto: CreateProductDto) {
+        return this.productsService.create(createProductDto)
     }
 
     @Delete(':id')
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    private products = []
+
+    getAll() {
+        return this.products
+    }
+
+    getById(id) {
+        return this.products.find(p => p.id === id)
+    }
+
+    create(productCreate: CreateProductDto) {
+        this.products.push({
+            ...productCreate,
+            id: Date.now().toString()
+        })
+        return this.products
+    }
+}
